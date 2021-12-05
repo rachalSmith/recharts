1bbd5fbe65feba05289338517dc8bd3ba5929513
rachalsmith <66318063+rachalSmith@users.noreply.github.com>
2021-12-05 10:28:42 +0000

    update to components

--- src/App.js
+++ src/App.js
@@ -1,69 +1,99 @@
-import Header from "./components/Header";
-import ToDoList from "./components/ToDoList";
-import AddToDo from "./components/AddToDo";
+import './App.css';
+import BarChartComponent from './components/BarChartComponent';
+import LineChartComponent from './components/LineChartComponent';
+import Header from './components/Header';
 
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
+
 
 function App() {
-// State and set state of todo list
-  const [toDoItems, setToDoItem] = useState([]);
 
-
-// Displays todo list from mock backend (json-server)
   useEffect(() => {
-    const getTodoList = async () => {
-      const toDoListFromServer = await fetchTodoList();
-      setToDoItem(toDoListFromServer);
+
+    const speciesURL = 'https://swapi.dev/api/species';
+    const planetsURL = 'https://swapi.dev/api/planets';
+
+
+//  Returns a raw array of data from API
+    const fetchSpecies = async () => {
+      try {
+        const response = await fetch(speciesURL);
+        const data = await response.json();
+
+        const { results } = data;
+
+        parsedSpeciesData(results);
+      }
+      catch(error) {
+        console.log('error', error);
+      }
     }
-    getTodoList();
+    fetchSpecies();
+
+
+//  Returns a raw array of data from API
+    const fetchPlanets = async () => {
+      try {
+
+        const response = await fetch(planetsURL);
+        const data = await response.json();
+
+        const { results } = data;
+
+        parsedPlanetData(results);
+      }
+      catch(error) {
+        console.log('error', error);
+      }
+    }
+    fetchPlanets();
+
+
+//  Returns parsed data for species and sets data as state.
+    const parsedSpeciesData = (results) => {
+
+      const cleanData = results.map(species => {
+        return {
+          name: species.name,
+          averageHeight: Number(species.average_height)
+        };
+      }).filter(species => !isNaN(species.averageHeight));
+      cleanData.splice(5, 4);
+
+      setBarData(cleanData);
+    }
+
+
+//  Returns parsed data for species and sets data as state.
+    const parsedPlanetData = (results) => {
+
+      const cleanData = results.map(planets => {
+        return {
+          name: planets.name,
+          diameter: (planets.diameter) / 1000,
+          day: planets.rotation_period,
+        };
+      }).filter(planets => planets.diameter <= 100);
+
+      setLineData(cleanData);
+    }
+
   }, [])
 
 
-// Fetches todo list from mock backend (json-server)
-const fetchTodoList = async () => {
-  const response = await fetch('http://localhost:5000/toDoList');
-  const data = await response.json();
-  return data;
-}
-
-
-// Add todo item to mock backend (json-server)
-const addToDoItem = async (item) => {
-  const response = await fetch('http://localhost:5000/toDoList', {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json'
-    },
-    body: JSON.stringify(item)
-  });
-
-  const data = await response.json();
-  console.log(data);
-
-  setToDoItem([...toDoItems, data]);
-}
-
-
-// Deletes todo text from mock backend (json-server)
-const deleteToDoItem = async (id) => {
-  await fetch(`http://localhost:5000/toDoList/${id}`, {
-    method: 'DELETE'
-  });
-// Creates new array of todo items.
-// When delete button is clicked, onDelete uses the todo id to identify todo.
-// The new array is a list of todo itemes that do not have the id of item clicked on.
-  setToDoItem(toDoItems.filter((toDo) => toDo.id !== id));
-}
+  const [barData, setBarData] = useState({});
+  const [lineData, setLineData] = useState({});
 
 
   return (
-    <div className='container'>
-      <div className='triangle'></div>
+    <div className="container">
       <Header />
-      <AddToDo onAdd={addToDoItem}/>
-      <ToDoList toDoItems={toDoItems} onDelete={deleteToDoItem}/>
+      <BarChartComponent BarChartData={barData}/>
+      <LineChartComponent LineChartData={lineData}/>
     </div>
   );
 }
 
 export default App;
+
+
