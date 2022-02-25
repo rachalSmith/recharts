d21cc55005040a585b984e79ca9ca18be1470a01
rachalsmith <66318063+rachalSmith@users.noreply.github.com>
2022-02-25 10:45:31 +0000

    msw testing

--- src/App.js
+++ src/App.js
@@ -9,80 +9,80 @@ import { useState, useEffect } from 'react';
 function App() {
 
   useEffect(() => {
-
-    const speciesURL = 'https://swapi.dev/api/species';
-    const planetsURL = 'https://swapi.dev/api/planets';
-
-
-//  Returns a raw array of data from API
-    const fetchSpecies = async () => {
-      try {
-        const response = await fetch(speciesURL);
-        const data = await response.json();
-
-        const { results } = data;
-
-        parsedSpeciesData(results);
-      }
-      catch(error) {
-        console.log('error', error);
-      }
-    }
     fetchSpecies();
-
-
-//  Returns a raw array of data from API
-    const fetchPlanets = async () => {
-      try {
-
-        const response = await fetch(planetsURL);
-        const data = await response.json();
-
-        const { results } = data;
-
-        parsedPlanetData(results);
-      }
-      catch(error) {
-        console.log('error', error);
-      }
-    }
     fetchPlanets();
-
-
-//  Returns parsed data for species and sets data as state.
-    const parsedSpeciesData = (results) => {
-
-      const cleanData = results.map(species => {
-        return {
-          name: species.name,
-          averageHeight: Number(species.average_height)
-        };
-      }).filter(species => !isNaN(species.averageHeight));
-      cleanData.splice(5, 4);
-
-      setBarData(cleanData);
-    }
-
-
-//  Returns parsed data for species and sets data as state.
-    const parsedPlanetData = (results) => {
-
-      const cleanData = results.map(planets => {
-        return {
-          name: planets.name,
-          diameter: (planets.diameter) / 1000,
-          day: planets.rotation_period,
-        };
-      }).filter(planets => planets.diameter <= 100);
-
-      setLineData(cleanData);
-    }
-
-  }, [])
+   }, [])
 
 
   const [barData, setBarData] = useState({});
   const [lineData, setLineData] = useState({});
+
+
+  const speciesURL = 'https://swapi.dev/api/species';
+  const planetsURL = 'https://swapi.dev/api/planets';
+
+
+//  Returns a raw array of data from API
+  const fetchSpecies = async () => {
+    try {
+      const response = await fetch(speciesURL);
+      const data = await response.json();
+      const { results } = data;
+
+      console.log(results)
+
+      parsedSpeciesData(results);
+    }
+    catch(error) {
+      console.log('error', error);
+    }
+  }
+
+
+//  Returns a raw array of data from API
+  const fetchPlanets = async () => {
+    try {
+      const response = await fetch(planetsURL);
+      const data = await response.json();
+
+      const { results } = data;
+
+      parsedPlanetData(results);
+    }
+    catch(error) {
+      console.log('error', error);
+    }
+  }
+
+
+//  Returns parsed data for species and sets data as state.
+  const parsedSpeciesData = (results) => {
+
+    const cleanData = results.map(species => {
+      return {
+        name: species.name,
+        averageHeight: Number(species.average_height)
+      };
+    }).filter(species => !isNaN(species.averageHeight));
+    cleanData.splice(5, 4);
+
+    setBarData(cleanData);
+  }
+
+
+//  Returns parsed data for species and sets data as state.
+  const parsedPlanetData = (results) => {
+
+    const cleanData = results.map(planets => {
+      return {
+        name: planets.name,
+        diameter: (planets.diameter) / 1000,
+        day: planets.rotation_period,
+      };
+    }).filter(planets => planets.diameter <= 100);
+
+    setLineData(cleanData);
+  }
 
 
   return (
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import speciesRawDataMock from "./mocks/fetchSpeciesData.json";
 
-test('renders learn react link', () => {
+import { render, screen } from "@testing-library/react";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+
+
+const server = setupServer(
+  rest.get('https://swapi.dev/api/species', (req, res, ctx) => {
+      return res(
+          ctx.json(speciesRawDataMock)
+      );
+  })
+)
+
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+
+test('render some async data', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const speciesData = await screen.findByTestId('find-me');
+  expect(speciesData).toContain('Wookie')
 });
